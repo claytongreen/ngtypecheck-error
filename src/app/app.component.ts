d13e95ae7f6f86dec3b89560e26916e3b5fe51b0
claytongreen <claytron.g@gmail.com>
2021-06-25 18:11:07 -0700

    Add angular-imask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-latest';
+
+  mask?: any;
+  unmask?: boolean;
+
+  ngOnInit() {
+    this.mask = { mask: '+{1}(000)000-0000' };
+    this.unmask = true;
+  }
 }
